b8e6dc402d502ff06a8007fd33e6c1233a98f7b2
Emma Maikuri <maikuriemma@gmail.com>
2022-07-01 15:55:21 +0300

    Added msql properties and table in repository

--- src/main/java/com/emma/bookz/model/Product.java
+++ src/main/java/com/emma/bookz/model/Product.java
@@ -5,14 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
 @Builder
+@Table(name="Product")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
